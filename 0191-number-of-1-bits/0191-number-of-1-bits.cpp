6da0c0fb44c0c2eac6950d8ac0663076468605f4
harshbatham2444 <148743781+harshbatham2444@users.noreply.github.com>
2026-07-02 16:27:35 +0530

    Time: 0 ms (100%), Space: 8.2 MB (82.05%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -5,11 +5,11 @@ public:
         int count = 0;
         while(n!=0){
             if(n&1){
-                count++;
-
+             count++;
             }
             n=n>>1;
         }
         return count;
+        
     }
 };
