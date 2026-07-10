ec3995949bea2878da2df1c1a4e9ca9134904037
harshbatham2444 <148743781+harshbatham2444@users.noreply.github.com>
2026-07-11 02:48:03 +0530

    Time: 2 ms (37.88%), Space: 10 MB (38.26%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> subset;
+
+    void dfs(vector<int>& nums, int i) {
+        if (i == nums.size()) {
+            ans.push_back(subset);
+            return;
+        }
+
+        subset.push_back(nums[i]);
+        dfs(nums, i + 1);
+
+        subset.pop_back();
+        dfs(nums, i + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        dfs(nums, 0);
+        return ans;
+    }
+};
